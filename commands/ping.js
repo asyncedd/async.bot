81ea51762a247626fe1987bd9ab2e61d1b539f78
asyncedd <isynqquwu@proton.me>
2023-09-07 18:17:20 +0900

    feat: initialize ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,37 @@
 import { SlashCommandBuilder } from "discord.js";
 
+function HEXToVBColor(hex) {
+  return parseInt(hex.replace("#", "0x"), 16);
+}
+
 export default {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!"),
   async execute(interaction) {
-    await interaction.reply("Pong!");
+    let responseEmbed = {
+      color: HEXToVBColor("#FFFFFF"),
+      title: "Pong!",
+      fields: [
+        {
+          name: "Ping",
+          value: "Pinging...",
+        },
+      ],
+    };
+
+    const sent = await interaction.reply({
+      embeds: [responseEmbed],
+      fetchReply: true,
+    });
+
+    responseEmbed.fields = [
+      {
+        name: "Ping",
+        value: `${sent.createdTimestamp - interaction.createdTimestamp}ms`,
+      },
+    ];
+
+    sent.edit({ embeds: [responseEmbed] });
   },
 };
