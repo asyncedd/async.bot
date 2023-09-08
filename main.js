011062f7d5fd38b67705b4238593bffe8c2b9ee4
asyncedd <isynqquwu@proton.me>
2023-09-08 16:07:16 +0900

    feat: Modulaize the events

--- main.js
+++ main.js
@@ -59,38 +59,20 @@ loadDatabase();
 
 loadCommands();
 
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(
-      `No command matching '${interaction.commandName}' was found.`
-    );
-    return;
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  let event = await import(filePath);
+  event = event.default;
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-
-  try {
-    await command.execute(interaction, {
-      client: client,
-      mongooes: mongoose.connection.models,
-    });
-  } catch (error) {
-    console.error(error);
-
-    const errorMessage = "There was an error while executing this command!";
-
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({ content: errorMessage, ephemeral: true });
-    } else {
-      await interaction.reply({ content: errorMessage, ephemeral: true });
-    }
-  }
-});
+}
 
 client.login(process.env.DISCORD_TOKEN);
