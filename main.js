830cf620253837c85efbcfe78b620b53dc13d0a0
asyncedd <isynqquwu@proton.me>
2023-09-10 16:46:06 +0900

    refactor(main): Make the script non-async

--- main.js
+++ main.js
@@ -13,37 +13,33 @@ client.commands = new Collection();
 
 const commandFolders = fs.readdirSync("./interactions");
 
-async function loadCommands() {
-  for (const folder of commandFolders) {
-    try {
-      const commandFiles = fs
-        .readdirSync(`./interactions/${folder}`)
-        .filter((file) => file.endsWith(".js"));
+for (const folder of commandFolders) {
+  try {
+    const commandFiles = fs
+      .readdirSync(`./interactions/${folder}`)
+      .filter((file) => file.endsWith(".js"));
 
-      for (const file of commandFiles) {
-        if (file.endsWith(".js")) {
-          const filePath = path.join(
-            path.join(__dirname, `interactions/${folder}`),
-            file
+    for (const file of commandFiles) {
+      if (file.endsWith(".js")) {
+        const filePath = path.join(
+          path.join(__dirname, `interactions/${folder}`),
+          file
+        );
+        const { default: command } = await import(filePath);
+
+        if ("data" in command && "execute" in command) {
+          client.commands.set(command.data.name, command);
+        } else {
+          console.log(
+            `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
           );
-          const { default: command } = await import(filePath);
-
-          if ("data" in command && "execute" in command) {
-            client.commands.set(command.data.name, command);
-          } else {
-            console.log(
-              `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-            );
-          }
         }
       }
-    } catch (error) {
-      console.error("Error loading commands:", error);
     }
+  } catch (error) {
+    console.error("Error loading commands:", error);
   }
 }
-
-loadCommands();
 
 const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs
