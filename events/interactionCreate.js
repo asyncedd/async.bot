0cf8d640422d0dedea54d7ecf19543cfd3bf1399
asyncedd <isynqquwu@proton.me>
2023-09-10 14:01:26 +0900

    feat: Add user information context menu

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,9 +3,8 @@ import { Events } from "discord.js";
 export default {
   name: Events.InteractionCreate,
   async execute(interaction, client) {
+    const command = interaction.client.commands.get(interaction.commandName);
     if (interaction.isChatInputCommand()) {
-      const command = interaction.client.commands.get(interaction.commandName);
-
       if (!command) {
         console.error(
           `No command matching '${interaction.commandName}' was found.`
@@ -19,6 +18,25 @@ export default {
         console.error(error);
 
         const errorMessage = "There was an error while executing this command!";
+
+        if (interaction.replied || interaction.deferred) {
+          await interaction.followUp({
+            content: errorMessage,
+            ephemeral: true,
+          });
+        } else {
+          await interaction.reply({ content: errorMessage, ephemeral: true });
+        }
+      }
+    }
+    if (interaction.isUserContextMenuCommand()) {
+      try {
+        await command.execute(interaction, { client: client });
+      } catch (error) {
+        console.error(error);
+
+        const errorMessage =
+          "There was an error while executing this interaction!";
 
         if (interaction.replied || interaction.deferred) {
           await interaction.followUp({
---
+++ interactions/context/information.js
@@ -0,0 +1,31 @@
+import { ContextMenuCommandBuilder, ApplicationCommandType } from "discord.js";
+
+function HEXToVBColor(hex) {
+  return parseInt(hex.replace("#", "0x"), 16);
+}
+
+export default {
+  data: new ContextMenuCommandBuilder()
+    .setName("User information")
+    .setType(ApplicationCommandType.User),
+  async execute(interaction) {
+    let user = interaction.targetUser;
+    let responseEmbed = {
+      color: HEXToVBColor("#FFFFFF"),
+      title: `Information for ${user.username}!`,
+      fields: [
+        {
+          name: "Username",
+          value: user.username,
+        },
+        {
+          name: "ID",
+          value: user.id,
+          inline: true,
+        },
+      ],
+    };
+
+    interaction.reply({ embeds: [responseEmbed] });
+  },
+};
